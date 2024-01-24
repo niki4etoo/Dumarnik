05d9f3715fc78227f8e33f18eba34f595d463042
Nikolai Nanev <niki4etooo@gmail.com>
2024-01-24 22:43:39 +0200

    added simple example of word interface in a dictionary

---
+++ Dumarnik/src/components/Dictionary/Words.tsx
@@ -0,0 +1,13 @@
+interface WordDictionary {
+    id: number;
+    word: string;
+    points: number;
+    length: number;
+}
+
+const exampleWord: WordDictionary = {
+    id: 1,
+    word: "example",
+    points: 10,
+    length: 7,
+  };
