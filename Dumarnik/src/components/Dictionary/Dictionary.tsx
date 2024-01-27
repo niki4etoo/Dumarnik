c2cf11904b657708835ef11b85dc6cccc96bc866
Nikolai Nanev <niki4etooo@gmail.com>
2024-01-27 22:30:42 +0200

    added all chunks of the bulgarian words into 16 separated json files

--- Dumarnik/src/components/Dictionary/Dictionary.tsx
+++ Dumarnik/src/components/Dictionary/Dictionary.tsx
@@ -1,13 +1,44 @@
 
-import * as wordsJSON from './dictionary_chunk1.json'
+import * as chunk_1 from './Chunks/dictionary_chunk1.json'
+import * as chunk_2 from './Chunks/dictionary_chunk2.json'
+import * as chunk_3 from './Chunks/dictionary_chunk3.json'
+import * as chunk_4 from './Chunks/dictionary_chunk4.json'
+import * as chunk_5 from './Chunks/dictionary_chunk5.json'
+import * as chunk_6 from './Chunks/dictionary_chunk6.json'
+import * as chunk_7 from './Chunks/dictionary_chunk7.json'
+import * as chunk_8 from './Chunks/dictionary_chunk8.json'
+import * as chunk_9 from './Chunks/dictionary_chunk9.json'
+import * as chunk_10 from './Chunks/dictionary_chunk10.json'
+import * as chunk_11 from './Chunks/dictionary_chunk11.json'
+import * as chunk_12 from './Chunks/dictionary_chunk12.json'
+import * as chunk_13 from './Chunks/dictionary_chunk13.json'
+import * as chunk_14 from './Chunks/dictionary_chunk14.json'
+import * as chunk_15 from './Chunks/dictionary_chunk15.json'
+import * as chunk_16 from './Chunks/dictionary_chunk16.json'
 
 class Dictionary {
 
-    words: string[] = wordsJSON.words;
+    words1: string[] = chunk_1.words;
+    words2: string[] = chunk_2.words;
+    words3: string[] = chunk_3.words;
+    words4: string[] = chunk_4.words;
+    words5: string[] = chunk_5.words;
+    words6: string[] = chunk_6.words;
+    words7: string[] = chunk_7.words;
+    words8: string[] = chunk_8.words;
+    words9: string[] = chunk_9.words;
+    words10: string[] = chunk_10.words;
+    words11: string[] = chunk_11.words;
+    words12: string[] = chunk_12.words;
+    words13: string[] = chunk_13.words;
+    words14: string[] = chunk_14.words;
+    words15: string[] = chunk_15.words;
+    words16: string[] = chunk_16.words;
+
 
     constructor() {
         
-        for(let i = 0; i < this.words.length; i++){
+        for(let i = 0; i < this.words1.length; i++){
             //to do
         }
     }
